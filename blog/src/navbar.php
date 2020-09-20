3e60a5a3f18da7c337a100aaf22deb9cbb4bd409
Orlando Rodriguez <orlandorodriguezosr@protonmail.ch>
2020-09-20 09:43:19 -0700

    Updated container spacing

--- blog/src/navbar.php
+++ blog/src/navbar.php
@@ -1,7 +1,7 @@
 <?php
 echo '<nav>
         <ul>
-				<li><a href="https://www.orlandorodriguez.xyz" style="font-family:cloister_black;color:lightgray;font-size:6em;border:0px;padding:0px;">O</a></li>
+				<li><a href="https://www.blog.orlandorodriguez.xyz" style="font-family:cloister_black;color:lightgray;font-size:6em;border:0px;padding:0px;">O</a></li>
 				<hr><p>Navigation</p>
                 <li><a href="https://www.orlandorodriguez.xyz">Main Site</a></li>
         </ul>
