aa677293ff2dd5f8cdf2c4f4ee108cd8ccbf5c5d
Orlando Rodriguez <orlandorodriguezosr@protonmail.ch>
2020-11-10 09:53:39 -0700

    edits

--- orlie/src/navbar.php
+++ orlie/src/navbar.php
@@ -12,22 +12,3 @@
 				<li><a href="https://github.com/OrlieRod314" target="_blank">Github</a></li>
         </ul>
 </nav>
-<?php
-/*
-echo '<nav>
-        <ul>
-                <p style="padding-top: 2px;">Home</p>
-				<li><a href="https://www.orlandorodriguez.xyz" style="font-family:cloister_black;color:lightgray;font-size:6em;border:0px;padding:0px;">O</a></li>
-				<hr><p>Personal</p>
-				<li><a href="/meta/about-me.php">Me</a></li>
-				<li><a href="/meta/about-this-site.php">Site</a></li>
-				<hr><p>Other Sites</p>
-				<li><a href="https://blog.orlandorodriguez.xyz" target="_blank">Blog</a></li>
-				<li><a href="https://resume.orlandorodriguez.xyz" target="_blank">Resume</a></li>
-				<hr><p>Works</p>
-				<li><a href="https://github.com/OrlieRod314" target="_blank">Github</a></li>
-        </ul>
-</nav>'
-*/
-
-?>
